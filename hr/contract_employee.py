293d1e37d0587f1c11b19f8424f96d504efb78b9
Ghadage-Radhika <149257253+Ghadage-Radhika@users.noreply.github.com>
2025-03-18 11:02:23 +0530

    Create contract_employee.py

---
+++ hr/contract_employee.py
@@ -0,0 +1,16 @@
+from employee import Employee
+
+class ContractEmployee(Employee):
+    def __init__(self, employee_id, name, department, hourly_rate, hours_worked):
+        super().__init__(employee_id, name, department)
+        self._hourly_rate = hourly_rate
+        self._hours_worked = hours_worked
+    
+    def calculate_salary(self):
+        return self._hourly_rate * self._hours_worked
+    
+    def display_details(self):
+        super().display_details()
+        print(f"Hourly Rate: ${self._hourly_rate:.2f}")
+        print(f"Hours Worked: {self._hours_worked}")
+        print(f"Total Salary: ${self.calculate_salary():.2f}\n")
