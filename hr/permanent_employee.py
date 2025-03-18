039f8c77a44b11432816ca8637389c932e9a4715
Ghadage-Radhika <149257253+Ghadage-Radhika@users.noreply.github.com>
2025-03-18 11:01:44 +0530

    Create permanent_employee.py

---
+++ hr/permanent_employee.py
@@ -0,0 +1,16 @@
+from employee import Employee
+
+class PermanentEmployee(Employee):
+    def __init__(self, employee_id, name, department, basic_salary, bonus):
+        super().__init__(employee_id, name, department)
+        self._basic_salary = basic_salary
+        self._bonus = bonus
+    
+    def calculate_salary(self):
+        return self._basic_salary + self._bonus
+    
+    def display_details(self):
+        super().display_details()
+        print(f"Basic Salary: ${self._basic_salary:.2f}")
+        print(f"Bonus: ${self._bonus:.2f}")
+        print(f"Total Salary: ${self.calculate_salary():.2f}\n")
