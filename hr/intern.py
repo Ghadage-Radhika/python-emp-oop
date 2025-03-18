2d8e90324f921ac167a8b0a7930e80d6586f71d4
Ghadage-Radhika <149257253+Ghadage-Radhika@users.noreply.github.com>
2025-03-18 11:03:16 +0530

    Create intern.py

---
+++ hr/intern.py
@@ -0,0 +1,14 @@
+from employee import Employee
+
+class Intern(Employee):
+    def __init__(self, employee_id, name, department, stipend):
+        super().__init__(employee_id, name, department)
+        self._stipend = stipend
+    
+    def calculate_salary(self):
+        return self._stipend
+    
+    def display_details(self):
+        super().display_details()
+        print(f"Stipend: ${self._stipend:.2f}")
+        print(f"Total Salary: ${self.calculate_salary():.2f}\n")
